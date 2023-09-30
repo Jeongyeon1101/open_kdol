0a592030400c21beeb3424f701b00a2adfd42637
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-09-30 16:13:03 +0000

    [Add] end_user,adminのテーブル・ログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  # 管理者用devise
+
+  devise_for :end_users, skip: [:passwords], controllers: {
+    registrations: "end_user/registrations",
+    sessions: "end_user/sessions"
+  }
+  # ユーザ用devise
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
