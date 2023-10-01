c7ee7660b98f7bd5cfd1f5606633f1a111243c4d
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-10-01 03:50:08 +0000

    [Add]　admin　topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   # ユーザ用devise
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+   root to: 'homes#top'
+  end
 
 end
