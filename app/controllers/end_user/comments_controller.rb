a0afae1a0fa5b47f7c09e108a80c52a672f212f7
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-10-17 15:52:37 +0000

    [Add] レイアウト調整 end_user(post_contents,comments,layouts)

--- app/controllers/end_user/comments_controller.rb
+++ app/controllers/end_user/comments_controller.rb
@@ -4,13 +4,13 @@ class EndUser::CommentsController < ApplicationController
   def create
     @post_content = PostContent.find(params[:post_content_id])
     @comment = current_end_user.comments.new(comment_params)
-    @comment.post_content_id = post_content.id
+    @comment.post_content_id = @post_content.id
     if @comment.save
-      @post_content.create_notification_comment!(current_end_user, comment_id)
-      redirect_to post_content_path(post_content)
-    else  
+      @post_content.create_notification_comment!(current_end_user, @comment.id)
+      redirect_to post_content_path(@post_content)
+    else
       render 'end_user/post_contents/show'
-    end  
+    end
   end
 
   def edit
