6414c1572b593d5c2ba916587dc7c14110e2344d
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-10-19 15:11:34 +0000

    [Add] レイアウト調整(all)

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -49,12 +49,12 @@ class EndUser < ApplicationRecord
     end
   end
 
-  def get_profile_image(width, height)
+  def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.png')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image
   end
 end
 # プロフィール画像を設定していない場合にデフォルトでno_imageが表示される
