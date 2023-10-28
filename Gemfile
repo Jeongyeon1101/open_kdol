11759f00ba1d60d5f44cd5351d10c2a4adf004fd
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-10-28 14:57:35 +0000

    {Add] レイアウト調整・バグ修正

--- Gemfile
+++ Gemfile
@@ -57,4 +57,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'simple_calendar', '~> 2.0'
 gem 'kaminari','~> 1.2.1'
-
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module OpenKdol
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    #　以下の記述を追記する(設定必須)
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
