5f5f726b503ef31125d7a4678db959b10d761239
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-12-20 12:09:25 +0000

    before_action :authenticate_admin! を追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin! 
   def top
     if params[:search_end_users]
       @end_users = EndUser.where("name LIKE ? ", '%' + params[:search_end_users] + '%')
